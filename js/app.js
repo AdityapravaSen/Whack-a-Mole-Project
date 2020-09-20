a428380a67f133537608a6a79071dd97432dcff3
Adityaprava Sen <adityapravasen0911@gmail.com>
2020-09-20 20:42:39 +0530

    disabled start while playing

--- js/app.js
+++ js/app.js
@@ -1,15 +1,20 @@
 // variable declarations
 
 const holes = document.querySelectorAll('.hole');
-const scorecard =document.querySelector('.score');
+const scorecard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
 const start = document.querySelector('button');
+const restart = document.querySelector('.reload');
 
 let lastMole;
 let timeUp = false;
-let points=0;
+let points = 0;
+let c = 0;
 
-//console.log(holes);
+//function to check setTimeout()
+function count() {
+    console.log(++c);
+}
 
 //function to set time for mole to stay up
 function randomTime() {
@@ -47,23 +52,50 @@ function molePop() {
     }, time);
 }
 
+//function to disable start wile game is running
+function disableStart() {
+    start.disabled = true;
+
+    console.log("disabled");
+
+    setTimeout(() => {
+        start.disabled = false;
+    }, 15000);
+}
+
 //function to start game
 function startGame() {
-    scorecard.textContent=0;
+    scorecard.textContent = 0;
     timeUp = false;
-    points=0;
+    points = 0;
 
+    disableStart();
     molePop();
-    setTimeout(() => timeUp = true, 10000);   //moles are displayed for 10 seconds straight
+
+    setTimeout(() => {
+        timeUp = true;
+
+        count();
+    }, 15000);   //moles are displayed for 15 seconds straight
 }
+
+// //function to restart the game
+// function restartGame() {
+//     if (window.confirm("Do you Want to Reload the Game?")) {
+//         window.location.reload();
+//     }
+
+// }
 
 start.addEventListener('click', startGame);    //starts the game
 
+// restart.addEventListener('click', restartGame); //restarts the game
+
 function hit(e) {
-    if(!e.isTrusted) return ;  //in case the mouse click is simulated 
-    points+=10;
+    if (!e.isTrusted) return;  //in case the mouse click is simulated 
+    points += 10;
     this.classList.add('mole');
-    scorecard.textContent=points;
+    scorecard.textContent = points;
 }
 
 moles.forEach(mole => mole.addEventListener('click', hit));
